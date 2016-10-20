346306a3ec0bb98a0db8fe111febc7719ad2117a
valeriocos <valerio.cosentino@gmail.com>
2016-10-20 10:12:47 +0200

    updating test for linux

--- test/test_linux.py
+++ test/test_linux.py
@@ -13,22 +13,30 @@ CONFIG = {
             'buffered': True
         }
 
-REFERENCES = ["origin/master"]
 
-
-def main():
+def test_1():
     g = Gitana(CONFIG, None)
     g.delete_previous_logs()
     g.init_db("linux_db")
 
     g.create_project("linux_db", "linux_project")
-    g.import_git_data("linux_db", "linux_project", "linux_repo", "C:\\Users\\atlanmod\\Desktop\\linux", None, 1, None, 20)
+    g.import_git_data("linux_db", "linux_project", "linux_repo", "C:\\Users\\atlanmod\\Desktop\\linux", None, 1, ["v2.6.19"], 20)
 
-    #g.import_bugzilla_tracker_data("papyrus_xxx", "papyrus", "papyrus_xxx", "https://bugs.eclipse.org/bugs/xmlrpc.cgi", "papyrus", None, False, 20)
-    #g.update_bugzilla_tracker_data("test_test", "test_project1", "test_repo1", "https://bugs.eclipse.org/bugs/xmlrpc.cgi", "papyrus", 20)
 
-    #g.import_eclipse_forum_data("papyrus_db", "papyrus", "https://www.eclipse.org/forums/index.php/f/121/", None, False, 4)
-    #g.update_eclipse_forum_data("test_test", "test_project1", "https://www.eclipse.org/forums/index.php/f/121/", 1)
+def test_2():
+    g = Gitana(CONFIG, None)
+    g.delete_previous_logs()
+    g.init_db("linux_db")
+
+    g.create_project("linux_db", "linux_project")
+    g.import_git_data("linux_db", "linux_project", "linux_repo", "C:\\Users\\atlanmod\\Desktop\\linux", None, 1, ["v2.6.14-rc1"], 20)
+
+
+def main():
+    #print "starting 1.."
+    #test_1()
+    print "starting 2.."
+    test_2()
 
 if __name__ == "__main__":
     main()
